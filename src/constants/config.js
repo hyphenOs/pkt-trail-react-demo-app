9443566f41e8390c8b55a6dba1f5f8c3edb1fa01
Mayur Borse <mayur@hyphenos.io>
2020-08-20 13:41:55 +0530

    WS server can be configured via settings form
    - App toolbar added with connect/disconnect, start/stop settings feature
    - Configurable WS server added to app config.
    - WS server can be changed via settings form
    - styled-components used for styling

---
+++ src/constants/config.js
@@ -0,0 +1,18 @@
+const config = {
+  appConfig: {
+    websocketServer: "ws://localhost:3030",
+  },
+  dashboardConfig: {
+    showSelectedDetails: true,
+  },
+  tableConfig: {
+    packetWindowSize: 50,
+    jumpSize: 1,
+    autoscroll: true,
+  },
+  detailsConfig: {
+    expanded: false,
+  },
+};
+
+export default config;
